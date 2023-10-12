89310464a23cd89a6fdd0d7af2a95111be5304fa
Jorge Morin <jmorin1@hawk.iit.edu>
2023-10-12 12:25:14 -0500

    Can record transcription and send to backend, need to figure out how to show on front end

---
+++ src/app/core/services/voice-service/voice.service.ts
@@ -0,0 +1,120 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+declare var webkitSpeechRecognition: any;
+declare var MediaRecorder: any;
+
+@Injectable({
+  providedIn: 'root',
+})
+
+export class VoiceService {
+  recognition = new webkitSpeechRecognition();
+  isStoppedSpeechRecog = false;
+  public text = '';
+  tempWords: any;
+
+  records$ = new BehaviorSubject(undefined);
+  recording = null;
+  mediaRecorder: any;
+  mediaObserver: any;
+  audio: any;
+  stream: any;
+
+  constructor() {}
+
+  init() {
+    this.recognition.interimResults = true;
+    this.recognition.lang = 'en-US';
+
+    this.recognition.addEventListener('result', (e: any) => {
+      const transcript = Array.from(e.results)
+        .map((result: any) => result[0])
+        .map((result) => result.transcript)
+        .join('');
+      this.tempWords = transcript;
+    });
+  }
+
+  start() {
+    this.socket = new WebSocket('ws://localhost:443/');
+    this.socket.onopen = (event) => {
+      console.log('WebSocket connection opened:', event);
+    };
+    this.isStoppedSpeechRecog = false;
+    this.recognition.start();
+    this.recognition.addEventListener('end', (condition: any) => {
+      
+      if (this.isStoppedSpeechRecog) {
+        this.recognition.stop();
+      } else {
+        this.wordConcat();
+        this.recognition.start();
+        this.onRecord();
+        if(this.text != ' .'){
+          this.text = this.text.substring(1);
+          this.socket.send(this.text);
+          console.log(this.text);
+        }
+
+        this.text = '';
+      }
+    });
+  }
+  stop() {
+    this.isStoppedSpeechRecog = true;
+    this.wordConcat();
+    this.recognition.stop();
+
+    this.mediaRecorder.stop();
+    setTimeout(
+      () => this.stream.getTracks().forEach((track: any) => track.stop()),
+      0
+    );
+  }
+
+  wordConcat() {
+    this.text = this.text + ' ' + this.tempWords + '.';
+    this.tempWords = '';
+  }
+
+  getRecords() {
+    return this.records$.asObservable();
+  }
+
+  getUserText(){
+    return this.text;
+  }
+
+  private socket!: WebSocket;
+  message: string = '';
+
+  onRecord() {
+    
+    navigator.mediaDevices.getUserMedia({ audio: true }).then((stream) => {
+      this.stream = stream;
+      this.mediaRecorder = new MediaRecorder(stream);
+      this.mediaRecorder.start();
+
+      const audioChunks: any = [];
+      this.mediaRecorder.addEventListener('dataavailable', (event: any) => {
+        audioChunks.push(event.data);
+      });
+
+      this.mediaRecorder.addEventListener('stop', () => {
+        const audioBlob = new Blob(audioChunks);
+        const audioUrl = URL.createObjectURL(audioBlob);
+        this.audio = new Audio(audioUrl) as HTMLAudioElement;
+
+        let newSet: any;
+        if (this.records$.getValue()) {
+          newSet = this.records$.getValue();
+          newSet.push({ media: this.audio, label: this.text });
+          this.records$.next(newSet);
+        }
+        //this.records$.next(null);
+        console.log('stopped recording');
+      });
+    });
+  }
+}
--- src/app/features/pages/speak-with-ai/speak-with-ai.component.ts
+++ src/app/features/pages/speak-with-ai/speak-with-ai.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
-import * as RecordRTC from 'recordrtc';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { VoiceService } from 'src/app/core/services/voice-service/voice.service';
 
 @Component({
   selector: 'app-speak-with-ai',
@@ -8,44 +8,30 @@ import * as RecordRTC from 'recordrtc';
   styleUrls: ['./speak-with-ai.component.css'],
 })
 export class SpeakWithAiComponent {
-  title = 'micRecorder';
-  record: any;
-  recording = false;
-  url: string | undefined;
-  error: string | undefined;
-  constructor(private domSanitizer: DomSanitizer) {}
-  sanitize(url: string) {
-    return this.domSanitizer.bypassSecurityTrustUrl(url);
+  records$: any;
+  audioRecording: any;
+  userText!: string;
+
+  message: string = '';
+
+  constructor(public service: VoiceService) {
+    this.service.init();
   }
-  initiateRecording() {
-    this.recording = true;
-    let mediaConstraints = {
-      video: false,
-      audio: true,
-    };
-    navigator.mediaDevices
-      .getUserMedia(mediaConstraints)
-      .then(this.successCallback.bind(this), this.errorCallback.bind(this));
+
+  ngOnInit() {
+    this.records$ = this.service.getRecords();
   }
-  successCallback(stream: any) {
-    var options = {
-      mimeType: 'audio/wav',
-      numberOfAudioChannels: 1,
-      sampleRate: 16000,
-    };
-    var StereoAudioRecorder = RecordRTC.StereoAudioRecorder;
-    this.record = new StereoAudioRecorder(stream);
-    this.record.record();
+
+  ngDoCheck() {
+
   }
-  stopRecording() {
-    this.recording = false;
-    this.record.stop(this.processRecording.bind(this));
+
+  startService() {
+    this.service.start();
   }
-  processRecording(blob: Blob | MediaSource) {
-    this.url = URL.createObjectURL(blob);
+
+  stopService() {
+    this.service.stop();
   }
-  errorCallback(error: any) {
-    this.error = 'Can not play audio in your browser';
-  }
-  ngOnInit() {}
+
 }

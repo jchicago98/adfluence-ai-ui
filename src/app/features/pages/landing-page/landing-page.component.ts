e1f921222ab2f25a1b8510e2339103038d2fcf0a
Jorge Morin <jmorin1@hawk.iit.edu>
2023-10-26 15:21:06 -0500

    Added chatbox in main landing page

--- src/app/features/pages/landing-page/landing-page.component.ts
+++ src/app/features/pages/landing-page/landing-page.component.ts
@@ -7,11 +7,6 @@ import { Router } from '@angular/router';
   styleUrls: ['./landing-page.component.css'],
 })
 export class LandingPageComponent {
-  mouseX: number = 0;
-  mouseY: number = 0;
-  highlightX: number = 0;
-  highlightY: number = 0;
-  highlightColor: string = 'transparent'; // Initialize with a transparent background
 
   constructor(private router: Router) {}
 

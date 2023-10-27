b109571dc569245303ee00d90a437a3bb3ccdc96
Jorge Morin <jmorin1@hawk.iit.edu>
2023-10-27 14:25:31 -0500

    Revamped chat view, can now scroll within live chat messages

--- src/app/features/pages/landing-page/landing-page.component.ts
+++ src/app/features/pages/landing-page/landing-page.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { VoiceService } from 'src/app/core/services/voice-service/voice.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-landing-page',
@@ -7,13 +9,54 @@ import { Router } from '@angular/router';
   styleUrls: ['./landing-page.component.css'],
 })
 export class LandingPageComponent {
+  listOfMessages: any[] = [];
+  private socket!: WebSocket;
+  private speechSynthesis: SpeechSynthesis;
+  websocketURL: string = environment.chatgptWebSocketURL;
+  uuidv4Pattern =
+    /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-4[0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$/;
+  clientId!: string;
 
-  constructor(private router: Router) {}
-
-  ngOnInit() {}
-
-  routeToSpeakWithAIPage(){
-    this.router.navigate(['/speak']);
+  constructor(public service: VoiceService, private http: HttpClient) {
+    this.service.init();
+    this.speechSynthesis = window.speechSynthesis;
   }
 
+  ngOnInit() {
+    this.socket = new WebSocket(this.websocketURL);
+  }
+
+  ngDoCheck() {
+    this.socket.onmessage = (event) => {
+      if (!this.uuidv4Pattern.test(event.data)) {
+        const message = JSON.parse(event.data);
+        console.log(message);
+        this.listOfMessages.push(message);
+        if (message.ai) {
+          this.speak(message.ai);
+        }
+      } else {
+        this.clientId = event.data;
+        this.service.updateClientId(this.clientId);
+      }
+    };
+  }
+
+  speak(text: string) {
+    this.service.isSystemSpeaking = true;
+    const utterance = new SpeechSynthesisUtterance(text);
+    utterance.onend = () => {
+      this.service.isSystemSpeaking = false;
+      this.startService();
+    };
+    this.speechSynthesis.speak(utterance);
+  }
+
+  startService() {
+    this.service.start();
+  }
+
+  stopService() {
+    this.service.stop();
+  }
 }
--- src/app/features/pages/speak-with-ai/speak-with-ai.component.ts
+++ src/app/features/pages/speak-with-ai/speak-with-ai.component.ts
@@ -1,11 +1,6 @@
 import { Component } from '@angular/core';
-import {
-  HttpClient,
-  HttpErrorResponse,
-  HttpHeaders,
-} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { VoiceService } from 'src/app/core/services/voice-service/voice.service';
-import { Observable, catchError, map, throwError } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
 @Component({
@@ -16,50 +11,11 @@ import { environment } from 'src/environments/environment';
 export class SpeakWithAiComponent {
   listOfMessages: any[] = [];
   private socket!: WebSocket;
-  voiceId = '21m00Tcm4TlvDq8ikWAM'; // Rachel Voice
   private speechSynthesis: SpeechSynthesis;
   websocketURL: string = environment.chatgptWebSocketURL;
   uuidv4Pattern =
     /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-4[0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$/;
   clientId!: string;
-
-  elevenLabsHeaders = {
-    headers: new HttpHeaders({
-      'Content-Type': 'audio/mpeg',
-    }),
-  };
-
-  textToSpeech(text: string): Observable<ArrayBuffer> {
-    const schema = {
-      text: text,
-      model_id: 'eleven_monolingual_v1',
-      xi_api_key: 'ELEVEN_LABS_API_KEY',
-      voice_settings: {
-        stability: 0,
-        similarity_boost: 0,
-        style: 0,
-        use_speaker_boost: true,
-      },
-    };
-
-    const headers = new HttpHeaders({
-      'Content-Type': 'application/json',
-    });
-
-    return this.http
-      .post(
-        'https://api.elevenlabs.io/v1/text-to-speech/' + this.voiceId,
-        schema,
-        {
-          responseType: 'arraybuffer',
-          headers: headers,
-        }
-      )
-      .pipe(
-        map((res) => res),
-        catchError(this.handleError)
-      );
-  }
 
   constructor(public service: VoiceService, private http: HttpClient) {
     this.service.init();
@@ -88,35 +44,15 @@ export class SpeakWithAiComponent {
     };
   }
 
-  elevenLabsTextToSpeech(message: string) {
-    this.textToSpeech(message).subscribe((data) => {
-      const audioContext = new window.AudioContext();
-      audioContext.decodeAudioData(
-        data,
-        (buffer) => {
-          const source = audioContext.createBufferSource();
-          source.buffer = buffer;
-          source.connect(audioContext.destination);
-          source.start(0);
-        },
-        (error) => {
-          console.error('Error decoding audio data', error);
-        }
-      );
-    });
-  }
-
   speak(text: string) {
-    this.service.isSystemSpeaking = true; 
+    this.service.isSystemSpeaking = true;
     const utterance = new SpeechSynthesisUtterance(text);
     utterance.onend = () => {
       this.service.isSystemSpeaking = false;
       this.startService();
     };
     this.speechSynthesis.speak(utterance);
-    
   }
-  
 
   startService() {
     this.service.start();
@@ -124,19 +60,5 @@ export class SpeakWithAiComponent {
 
   stopService() {
     this.service.stop();
-  }
-
-  private handleError(error: HttpErrorResponse) {
-    if (error.status === 0) {
-      console.error('An error occurred:', error.error);
-    } else {
-      console.error(
-        `Backend returned code ${error.status}, body was: `,
-        error.error
-      );
-    }
-    return throwError(
-      () => new Error('Something bad happened; please try again later.')
-    );
   }
 }
